2e4ff527775a7eec4a16ae437381b84548b3ee3d
= <=>
2020-08-18 11:41:07 -0600

    Basic functionality added.

---
+++ scripts/script.ts
@@ -0,0 +1,137 @@
+const page = {
+    tiles: document.querySelectorAll('.tic-tac')!,
+    header: document.querySelector('h1')!,
+};
+
+/* Model */
+
+let playerMark: string = 'X';
+
+let board = (() => {
+    const _SIDE_LENGTH = 3;
+    const _MAX_MOVES = 9;
+    let _movesMade = 0;
+
+    let _board = [
+        ['','',''], 
+        ['','',''], 
+        ['','','']
+    ];
+    let _state = {
+        empty: '',
+        x: 'X',
+        o: 'O'
+    };
+
+    const _numberToPosition = (index: number) => {
+        return {
+            row: Math.floor(index / _SIDE_LENGTH), 
+            col: index % _SIDE_LENGTH
+        };
+    }
+
+    const _setState = (row: number, col: number, text: string) => {
+        _board[row][col] = text;
+    }
+
+     // Every element in array matches the first element.
+    const _isWinningRow = (arr: Array<string>) => {
+        return String(arr) == 
+               String(arr[0].repeat(_SIDE_LENGTH).split(''));
+    }
+
+    const _rowVictory = () => {
+        for (let row of _board) {
+            if (row.indexOf('') != -1) {
+                continue;
+            }
+            
+            if (_isWinningRow(row)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const _colVictory = () => {
+        for (let col in _board) {
+            let column: Array<string> = [];
+            _board.forEach((row) => column.push(row[col]));
+            
+            if (_isWinningRow(column)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const _diagVictory = () => {
+        let leftToRight = [];
+        let rightToLeft = [];
+        for (let i = 0; i < _SIDE_LENGTH; i++) {
+            leftToRight.push(_board[i][i])
+            rightToLeft.push(
+                _board[i][(_SIDE_LENGTH-1)-i]
+            )
+        }
+        
+        if (leftToRight.indexOf('') != -1 &&
+            rightToLeft.indexOf('') != -1
+        ){
+            return false;
+        }
+        
+        return _isWinningRow(leftToRight) || _isWinningRow(rightToLeft)
+    }
+
+    const _victoryCheck = () => {
+        return (
+            _rowVictory() ||
+            _colVictory() ||
+            _diagVictory()
+        );
+    };
+
+    const changeTile = (index: number, playerChoice: string) => {
+        if (_movesMade >= _MAX_MOVES) {
+            page.header.textContent = "The Game is Over!"
+            return '-';
+        }
+        
+        _movesMade += 1;
+        
+        let {row, col} = _numberToPosition(index);
+        let playerMark = _movesMade % 2? 'O' : 'X';
+        _setState(row, col, playerMark);
+        
+        // TODO
+        if (_victoryCheck()) {
+            page.header.textContent = `${playerMark} is The Winner!`;
+        }
+        
+        return playerMark;
+    }
+
+    return {changeTile};
+})();
+
+
+/* View */
+
+const tileUsed = (button: any, mark: string) => {
+    button.disable = true;
+    button.textContent = mark;
+}
+
+/* Controller */
+
+// Each button is clickable once.
+page.tiles.forEach((tile, index) => {
+    const makeMove = (e: Event | any) => {
+        playerMark = board.changeTile(index, playerMark);
+        tileUsed(e.target!, playerMark);
+
+        e.target!.removeEventListener('click', makeMove);
+    };
+    tile.addEventListener('click', makeMove);
+});
